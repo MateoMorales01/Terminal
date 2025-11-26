7c94a7bbf63f0c6c87047d25e0c09134d67d27d3
Mateo Morales <Moralmat76@gmail.com>
2025-11-26 13:12:55 -0600

    initial commit 2

--- src/main/java/com/pluralsight/Terminal.java
+++ src/main/java/com/pluralsight/Terminal.java
@@ -3,5 +3,6 @@ package com.pluralsight;
 public class Terminal {
     public static void main(String[] args) {
         System.out.println("Testing");
+        System.out.println("Another commit");
     }
 }
